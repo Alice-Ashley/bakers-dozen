4e6570cdc5c38f491c978070a605f5c6153cb3c2
Alice Ashley <alice.ashley@hotmail.co.uk>
2023-01-04 18:58:01 +0000

    JavaScript add - Page 2

--- page2.js
+++ page2.js
@@ -1,13 +1,3 @@
- window.addEventListener('click', event => {clickFunction(event);});
-
-function clickFunction(e){
-     console.log(e.target.value)
- } ;
-
-
-
-
-//step 1 - capture radio button values
-// save these to variables
-//write logic for when all three categories have been selected
-//display text after this
+function clickAlert() {
+       alert("Thank you for your order!")
+}
