13c8429a828186dab8df01b844ca706466a2505e
Alice Ashley <alice.ashley@hotmail.co.uk>
2023-01-04 12:44:30 +0000

    added image
    added image

---
+++ page2.js
@@ -0,0 +1,13 @@
+ window.addEventListener('click', event => {clickFunction(event);});
+
+function clickFunction(e){
+     console.log(e.target.value)
+ } ;
+
+
+
+
+//step 1 - capture radio button values
+// save these to variables
+//write logic for when all three categories have been selected
+//display text after this
